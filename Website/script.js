20cfc019000bec2d5e77719f493d857c1d8551c7
kaseyd <kasey.dean@bobcats.gcsu.edu>
2015-03-11 10:25:56 -0400

    CSSTesting
    CSS, HTML, and Javascript files for testing the styling, layout, and
    design of the website.

---
+++ Website/script.js
@@ -0,0 +1,12 @@
+document.getElementById("nav1").innerHTML=
+"<ul id='menu'>" +
+"<li><a href='home.html'>Home</a></li>" +
+"<li><a href='login.html'>Login</a></li>" +
+"<li><a href='about.html'>About</a></li>" +
+"<li><a href='contact.html'>Contact Us</a></li>" +
+"</ul>";
+
+document.getElementById("foot01").innerHTML= 
+"<ul id='ftr'>" +
+"<p>&copy; " + new Date().getFullYear() + " <a href='http://www.gcsu.edu/'>Georgia College & State University.</a> All rights reserved.</p>" +
+"</ul>";
